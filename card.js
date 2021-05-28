0ff880b38d5a07591a1ccc9ef072f9b90df44acc
Apostle-H <malinovski.nik@gmail.com>
2021-05-28 11:49:44 +0300

    hand

---
+++ card.js
@@ -0,0 +1,187 @@
+let cards = [
+    {
+        path: "cards/1.svg",
+        suit: "chervi",
+        rank: "A"
+    },
+    {
+        path: "cards/2.svg",
+        suit: "kresti",
+        rank: "A"
+    },
+    {
+        path: "cards/3.svg",
+        suit: "bubi",
+        rank: "A"
+    },
+    {
+        path: "cards/4.svg",
+        suit: "piki",
+        rank: "A"
+    },    {
+        path: "cards/5.svg",
+        suit: "chervi",
+        rank: "k"
+    },
+    {
+        path: "cards/6.svg",
+        suit: "kresti",
+        rank: "K"
+    },
+    {
+        path: "cards/7.svg",
+        suit: "bubi",
+        rank: "K"
+    },
+    {
+        path: "cards/8.svg",
+        suit: "piki",
+        rank: "K"
+    },
+    {
+        path: "cards/9.svg",
+        suit: "chervi",
+        rank: "Q"
+    },
+    {
+        path: "cards/10.svg",
+        suit: "kresti",
+        rank: "Q"
+    },
+    {
+        path: "cards/11.svg",
+        suit: "bubi",
+        rank: "Q"
+    },
+    {
+        path: "cards/12.svg",
+        suit: "piki",
+        rank: "Q"
+    },    
+    {
+        path: "cards/13.svg",
+        suit: "chervi",
+        rank: "J"
+    },
+    {
+        path: "cards/14.svg",
+        suit: "kresti",
+        rank: "J"
+    },
+    {
+        path: "cards/15.svg",
+        suit: "bubi",
+        rank: "J"
+    },
+    {
+        path: "cards/16.svg",
+        suit: "piki",
+        rank: "J"
+    },
+    {
+        path: "cards/17.svg",
+        suit: "chervi",
+        rank: "10"
+    },
+    {
+        path: "cards/18.svg",
+        suit: "kresti",
+        rank: "10"
+    },
+    {
+        path: "cards/19.svg",
+        suit: "bubi",
+        rank: "10"
+    },
+    {
+        path: "cards/20.svg",
+        suit: "piki",
+        rank: "10"
+    },
+    {
+        path: "cards/21.svg",
+        suit: "chervi",
+        rank: "9"
+    },
+    {
+        path: "cards/22.svg",
+        suit: "kresti",
+        rank: "9"
+    },
+    {
+        path: "cards/23.svg",
+        suit: "bubi",
+        rank: "9"
+    },
+    {
+        path: "cards/24.svg",
+        suit: "piki",
+        rank: "9"
+    },
+    {
+        path: "cards/25.svg",
+        suit: "chervi",
+        rank: "8"
+    },
+    {
+        path: "cards/26.svg",
+        suit: "kresti",
+        rank: "8"
+    },
+    {
+        path: "cards/27.svg",
+        suit: "bubi",
+        rank: "8"
+    },
+    {
+        path: "cards/28.svg",
+        suit: "piki",
+        rank: "8"
+    },
+    {
+        path: "cards/29.svg",
+        suit: "chervi",
+        rank: "7"
+    },
+    {
+        path: "cards/30.svg",
+        suit: "kresti",
+        rank: "7"
+    },
+    {
+        path: "cards/31.svg",
+        suit: "bubi",
+        rank: "7"
+    },
+    {
+        path: "cards/32.svg",
+        suit: "piki",
+        rank: "7"
+    },
+    {
+        path: "cards/33.svg",
+        suit: "chervi",
+        rank: "6"
+    },
+    {
+        path: "cards/34.svg",
+        suit: "kresti",
+        rank: "6"
+    },
+    {
+        path: "cards/35.svg",
+        suit: "bubi",
+        rank: "6"
+    },
+    {
+        path: "cards/36.svg",
+        suit: "piki",
+        rank: "6"
+    }
+];
+
+const series = ["A","6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+cards.sort(() => Math.random() - 0.5);
+
+// console.log(cards);
